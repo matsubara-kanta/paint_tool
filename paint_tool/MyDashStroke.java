4773df19bc9a0f92bbf5697d14946ed0c2cb4e93
matsubara-kanta <45473854+matsubara-kanta@users.noreply.github.com>
2022-04-07 00:59:59 +0900

    Delete paint_tool directory

--- paint_tool/MyDashStroke.java
+++
@@ -1,8 +0,0 @@
-import java.awt.BasicStroke;
-
-	public class MyDashStroke extends BasicStroke {
-
-		public MyDashStroke(float lineWidth,float[] pattern) {
-			super(lineWidth,CAP_BUTT,JOIN_BEVEL,1.0f,pattern,0);
-		}
-	}
